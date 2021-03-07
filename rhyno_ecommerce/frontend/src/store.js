6cb5cbd17a20d1243cbd7f99c04c890c6c5c80d6
ddawkins <ddawkns@gmail.com>
2021-03-07 12:13:12 -0700

    cart reducer and actions

--- rhyno_ecommerce/frontend/src/store.js
+++ rhyno_ecommerce/frontend/src/store.js
@@ -6,12 +6,21 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 
+import { cartReducer } from "./reducers/cartReducers";
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const intitalState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const intitalState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
