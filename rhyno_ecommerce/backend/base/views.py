e9844d7c4c2ef4625e80bab8f5665a2e0843cda7
ddawkins <ddawkns@gmail.com>
2021-03-08 08:02:41 -0700

    JWT Customization

--- rhyno_ecommerce/backend/base/views.py
+++ rhyno_ecommerce/backend/base/views.py
@@ -8,7 +8,24 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['eamil'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def getRoutes(request):
     return Response('Hello')
