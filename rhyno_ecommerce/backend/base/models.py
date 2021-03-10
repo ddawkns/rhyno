d05131e194f7308e406cc94800957d477a715c53
ddawkins <ddawkns@gmail.com>
2021-03-09 22:04:31 -0700

    Address Bug Fix

--- rhyno_ecommerce/backend/base/models.py
+++ rhyno_ecommerce/backend/base/models.py
@@ -62,7 +62,7 @@ class OrderItem(models.Model):
 
 class ShippingAddress(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True)
-    addres = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=200, null=True, blank=True)
     postalCode = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
@@ -70,4 +70,4 @@ class ShippingAddress(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.addres)    
+        return str(self.address)    
