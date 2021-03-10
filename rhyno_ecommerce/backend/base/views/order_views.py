df03cd38d79080e13b3ba0217eade25ad65e2bb8
ddawkins <ddawkns@gmail.com>
2021-03-09 19:40:54 -0700

    order_views and serializers bug fix

--- rhyno_ecommerce/backend/base/views/order_views.py
+++ rhyno_ecommerce/backend/base/views/order_views.py
@@ -10,7 +10,7 @@ from base.serializers import ProductSerializer, OrderSerializer
 from rest_framework import status
 
 @api_view(['POST'])
-@permission_classes(['IsAuthenticated'])
+@permission_classes([IsAuthenticated])
 def addOrderItems(request):
     user = request.user
     data = request.data
@@ -54,5 +54,5 @@ def addOrderItems(request):
             product.countInStock -= item.qty
             product.save()
     
-    serializer = OrderSerializer(order, many=True)
-    return Response(serializer.data)
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
