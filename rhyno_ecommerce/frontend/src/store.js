2df1a5b874a60fbdf4f074409ba89fdb365f2e2a
ddawkins <ddawkns@gmail.com>
2021-03-10 00:08:25 -0700

    Shipping Screen and Save Address

--- rhyno_ecommerce/frontend/src/store.js
+++ rhyno_ecommerce/frontend/src/store.js
@@ -34,8 +34,15 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const intitalState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: { shippingAddress: shippingAddressFromStorage },
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
